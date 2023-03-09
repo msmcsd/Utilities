6a3816c238acd78ee69775011f26b37491de8e73
Jason Shih <jshih@blackberry.com>
2023-03-08 17:44:30 -0800

    First check-in

---
+++ ManageBrowserTabs/GetChromeTabs.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using UIAutomationClient;
+using TreeScope = UIAutomationClient.TreeScope;
+
+namespace ManageBrowserTabs
+{
+    internal class GetChromeTabs
+    {
+        private readonly CUIAutomation _automation;
+
+        public GetChromeTabs()
+        {
+            _automation = new CUIAutomation();
+            _automation.AddFocusChangedEventHandler(null, new FocusChangeHandler(this));
+        }
+
+        public class FocusChangeHandler : IUIAutomationFocusChangedEventHandler
+        {
+            private readonly GetChromeTabs _listener;
+
+            public FocusChangeHandler(GetChromeTabs listener)
+            {
+                _listener = listener;
+            }
+
+            public void HandleFocusChangedEvent(IUIAutomationElement element)
+            {
+                if (element != null)
+                {
+                    Process[] processes = Process.GetProcessesByName("chrome");
+                    if (processes.Length <= 0)
+                    {
+                        Console.WriteLine("Chrome is not running");
+                    }
+                    else
+                    {
+                        foreach (Process process in processes)
+                        {
+                            try
+                            {
+                                IUIAutomationElement elm = this._listener._automation.ElementFromHandle(process.MainWindowHandle);
+                                IUIAutomationCondition Cond = this._listener._automation.CreatePropertyCondition(30003, 50004);
+                                IUIAutomationElementArray elm2 = elm.FindAll(TreeScope.TreeScope_Descendants, Cond);
+                                for (int i = 0; i < elm2.Length; i++)
+                                {
+                                    IUIAutomationElement elm3 = elm2.GetElement(i);
+                                    IUIAutomationValuePattern val = (IUIAutomationValuePattern)elm3.GetCurrentPattern(10002);
+                                    if (val.CurrentValue != "")
+                                    {
+                                        Console.WriteLine("URL found: " + val.CurrentValue);
+                                    }
+                                }
+                            }
+                            catch { }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
